bb3d04ac76f89475638747fd9fde7d1215442099
Janilson Varela <janilsonjvs@gmail.com>
2025-09-06 19:37:19 -0300

    feat: controller marca

--- src/main/java/br/com/plugsystem/presentation/controllers/MarcaController.java
+++ src/main/java/br/com/plugsystem/presentation/controllers/MarcaController.java
@@ -1,17 +1,26 @@
 package br.com.plugsystem.presentation.controllers;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.plugsystem.application.dto.marca.MarcaResponse;
+import br.com.plugsystem.application.service.marca.MarcaService;
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/marcas")
+@RequiredArgsConstructor
 public class MarcaController {
 
+    private final MarcaService service;
+
     @GetMapping
-    public String index()
+    public List<MarcaResponse> index()
     {
-        return "Marcas";
+        return service.list();
     }
     
 }

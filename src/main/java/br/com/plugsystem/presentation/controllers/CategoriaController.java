38aa60b877a778278a5cd1150e0558e9dcfd3385
Janilson Varela <janilsonjvs@gmail.com>
2025-09-06 21:11:22 -0300

    feat: categoria controller

--- src/main/java/br/com/plugsystem/presentation/controllers/CategoriaController.java
+++ src/main/java/br/com/plugsystem/presentation/controllers/CategoriaController.java
@@ -1,14 +1,24 @@
 package br.com.plugsystem.presentation.controllers;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.plugsystem.application.dto.categoria.CategoriaResponse;
+import br.com.plugsystem.application.service.categoria.CategoriaService;
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/categorias")
+@RequiredArgsConstructor
 public class CategoriaController {
+
+    private final CategoriaService service;
+
     @GetMapping
-    public String index() {
-        return "Categorias";
+    public List<CategoriaResponse> index() {
+        return service.list(); 
     }
 }
